1afb7c00f718ab6e3a6dd41ddb7389bddad5359c
srivallikilari <50615841+srivallikilari@users.noreply.github.com>
2019-05-14 14:20:07 +0530

    Add files via upload

---
+++ analoginput_output.ino
@@ -0,0 +1,16 @@
+int LEDpin=D6;
+void setup() {
+  Serial.begin(9600);
+  analogWrite(LEDpin,1023);
+
+}
+
+void loop() {
+  int dutycycle=analogRead(A0);
+  if(dutycycle>1023)dutycycle=1023;
+  Serial.print("Duty cycle: ");
+  Serial.println(dutycycle);
+  analogWrite(LEDpin,dutycycle);
+  delay(100);
+
+}
